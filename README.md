176248d6d96ce021ad538b0cf260d7c578ebb643
Tavish Aggawal <Tavish@Tavishs-MacBook-Air.local>
2016-10-27 12:01:59 +0530

    Debugging the code and run on server

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # nodeJS
+Node JS in simple words is JavaScript on server.
+Node JS involves asynchronous calls (non-blocking).
--- nodeJS/nodejs101.js
+++ nodeJS/nodejs101.js
@@ -1,2 +1,10 @@
-// Verifying node js installation
-console.log("Hello World!");
+//1. Import Required Module
+var http = require("http");
+
+//2. create server
+http.createServer(function(request,response){
+response.end("hello world");
+}).listen(8081);
+
+//3. Testing and running the response
+console.log('Server running at http://127.0.0.1:8081/');
