363976f32ea074dd13353454b70b97ecdcdfb3b5
Tavish Aggawal <Tavish@Tavishs-MacBook-Air.local>
2016-11-06 01:32:22 +0530

    CURD operation using MEAN stack

---
+++ meanstack/public/js_controller/controller.js
@@ -0,0 +1,49 @@
+var app = angular.module('myapp',[]);
+
+app.controller("myController",function($scope,$http){
+
+var refresh = function(){
+    $http.get("/contactlist").success(function(response){
+        //console.log(response);
+        $scope.contactlist = response;
+        $scope.contact="";
+    });
+};
+
+ refresh();
+
+    $scope.addContact = function(){
+        //console.log($scope.contact);  
+        $http.post("/contactlist",$scope.contact).success(function(response){
+             console.log(response);
+             refresh();
+         });
+    };
+
+    $scope.removeContact = function(id){
+       // console.log(id); 
+       $http.delete("/contactlist/" + id).success(function(response){
+             console.log(response);
+             refresh();
+         });
+    };
+
+    $scope.editContact = function(id){
+       // console.log(id); 
+       $http.get("/contactlist/"+id).success(function(response){
+         //console.log(response);
+            $scope.contact = response;
+        });
+    };
+
+    $scope.updateContact = function(){
+        console.log($scope.contact._id); 
+        var id = $scope.contact._id;
+
+        $http.put("/contactlist/"+id,$scope.contact).success(function(response){
+            $scope.contactlist = response;
+            refresh();
+        });
+    };
+
+});
